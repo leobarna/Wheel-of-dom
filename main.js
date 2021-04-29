335ba3e53d2213303288694d2de3c71d8f45059e
Olga Caparrós <caparrosolga@gmail.com>
2021-04-29 15:20:56 +0200

    voluntario

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const nombres = ["Alisa","Amanda","Ana G","Ana L", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sonia", "Taiza", "Yasmín"
 ];
-
-document.querySelector("button").addEventListener("click",voluntario);
-let positionRandom = Math.floor(Math.random()*nombres.length);
 function voluntario(){
-
+    let positionRandom = Math.floor(Math.random()*nombres.length);
     const randomItem = nombres[positionRandom];
     console.log(randomItem);
+    document.body.innerHTML = (randomItem);
 
 }
-function myDelete() {
-    nombres.splice(positionRandom,1)
-}
+
+document.querySelector("button").addEventListener("click",voluntario);
+//const content = element.innerHTML;
+
+
+//console.log(voluntario());
+//function myDelete() {
+   // nombres.splice(positionRandom,1)//
+//}
 //console.log(nombres[4]);
 
-console.log(voluntario());
-console.log(myDelete());
+
+//console.log(myDelete());
 
