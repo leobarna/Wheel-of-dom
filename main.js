9ac2950188ee24482c1e7d0dbf585cc8bdf499bc
Olga Caparrós <caparrosolga@gmail.com>
2021-04-29 11:46:04 +0200

    funciones click

---
+++ main.js
@@ -0,0 +1,15 @@
+const nombres = ["Alisa","Amanda","Ana G","Ana L", "Angie", "Clau", "Daniela R.", "Daniela T.", "Eliana", "Flo", "Gaby", "Leo", "Inga", "Pepi", "María Laura", "Marta", "Nicole", "Olga", "Yeraldin", "Ranju", "Rosangely", "Sonia", "Taiza", "Yasmín"
+];
+
+document.querySelector("button").addEventListener("click",voluntario);
+
+function voluntario(){
+    const randomItem = nombres[Math.floor(Math.random()*nombres.length)];
+    console.log(randomItem);
+
+}
+
+//console.log(nombres[4]);
+
+console.log(voluntario());
+
